bf582c1fd8742791f169056e50c0508e70f1a842
adam.haniif <adam.haniif@bluebirdgroup.com>
2022-09-21 22:53:19 +0700

    update katalon

---
+++ Jenkinsfile
@@ -0,0 +1,11 @@
+pipeline {
+    agent any
+    stages {
+        stage('run katalon from local') {
+            steps {
+                sh 'run_chrome_1.sh'
+            }
+        }
+    }
+
+}
---
+++ run_chrome_2.bat
@@ -0,0 +1,1 @@
+katalonc -noSplash -runMode=console -projectPath="C:\CDesktop\projek\jenkin\ci-samples-master\test.prj" -retry=0 -testSuitePath="Test Suites/TS_RegressionTest" -browserType="Chrome (headless)" -executionProfile="default" -apiKey="d7261927-28cc-4591-a4ae-e526900d2974" --config -proxy.auth.option=NO_PROXY -proxy.system.option=NO_PROXY -proxy.system.applyToDesiredCapabilities=true
